c5548d4cc33a60e3d403772fb3cc1c303fd0934d
Sawyer Hollenshead <s.hollenshead@gmail.com>
2012-06-22 15:20:54 -0700

    Style.css / Fix errors

--- header.php
+++ header.php
@@ -4,8 +4,29 @@
 
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1,<?php bloginfo( 'html_type' ); ?>">
-	
-	<title><?php shaken_title_tag(); ?></title>
+	<!-- <meta name="viewport" content="width=device-width" /> -->
+
+    <title><?php
+    /*
+     * Print the <title> tag based on what is being viewed.
+     */
+    global $page, $paged;
+
+    wp_title( '|', true, 'right' );
+
+    // Add the blog name.
+    bloginfo( 'name' );
+
+    // Add the blog description for the home/front page.
+    $site_description = get_bloginfo( 'description', 'display' );
+    if ( $site_description && ( is_home() || is_front_page() ) )
+        echo " | $site_description";
+
+    // Add a page number if necessary:
+    if ( $paged >= 2 || $page >= 2 )
+        echo ' | ' . sprintf( __( 'Page %s', 'twentyeleven' ), max( $paged, $page ) );
+
+    ?></title>
 	
 	<link rel="profile" href="http://gmpg.org/xfn/11" />
 	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
@@ -13,3 +34,9 @@
 </head>
 
 <body <?php body_class(); ?>>
+
+    <h1 class="logo">
+    <a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
+        <img src="<?php echo get_template_directory_uri(); ?>/images/logo.png" alt="<?php bloginfo( 'name' ); ?>" /></h1>
+    </a>
+    </h1>
